7c953807adffc8fddda1c03be479f9684a4bd019
kalle605 <kalle605@live.se>
2015-04-30 13:33:17 +0200

    Bort med golvpiezo!

--- src/main/datahandler/LineChart.java
+++ src/main/datahandler/LineChart.java
@@ -16,8 +16,7 @@ public class LineChart extends ApplicationFrame {
 	private static final long serialVersionUID = -8186205081849264360L;
 	public static final String[] NAMES = { "sound", "movement",
 			// "people",
-			"light", "piezo", "carbon dioxide", "piezo golv",
-			"Detected Presence" };
+			"light", "piezo", "carbon dioxide", "Detected Presence" };
 
 	public LineChart(String applicationTitle, String chartTitle,
 			XYSeriesCollection dataset, XYSeriesCollection dataset2) {
