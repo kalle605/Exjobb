fe351506717d8fa2cfefdf38e8dffb5f332a10b5
kalle605 <ndi12klh@08359-STUD-L.HIG-AD.hig.se>
2015-05-04 16:22:27 +0200

    comport

--- src/main/SerielCom.java
+++ src/main/SerielCom.java
@@ -37,7 +37,7 @@ public class SerielCom implements SerialPortEventListener {
 	public void initialize() {
 		CommPortIdentifier portId = null;
 		try {
-			portId = CommPortIdentifier.getPortIdentifier("COM3");
+			portId = CommPortIdentifier.getPortIdentifier("COM4");
 		} catch (NoSuchPortException e1) {
 			e1.printStackTrace();
 		}
