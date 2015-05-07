818ab21a3feafc2d713295b131b39f28f20792e8
kalle605 <ndi12klh@08359-STUD-L.HIG-AD.hig.se>
2015-05-07 12:36:38 +0200

    asddsa

---
+++ src/main/tests/asdasd.java
@@ -0,0 +1,14 @@
+package main.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class asdasd {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
