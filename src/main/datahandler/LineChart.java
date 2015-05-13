76996554e4eefa4ad1a3b90417d61e516e455fe4
kalle605 <kalle605@live.se>
2015-05-13 13:57:08 +0200

    asdads

--- src/main/datahandler/LineChart.java
+++ src/main/datahandler/LineChart.java
@@ -2,7 +2,6 @@ package main.datahandler;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.Stroke;
 import java.text.SimpleDateFormat;
 
 import org.jfree.chart.ChartFactory;
@@ -11,6 +10,7 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.ApplicationFrame;
 
@@ -27,15 +27,23 @@ public class LineChart extends ApplicationFrame {
 				"Time", "Serie", dataset);
 
 		XYPlot plot = (XYPlot) lineChart.getPlot();
+
 		final NumberAxis axis2 = new NumberAxis("Detected Presence");
-		axis2.setAxisLinePaint(Color.pink);
-		Stroke tickMarkLineStroke = new BasicStroke(1.4f,
-				BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER);
-		axis2.setAxisLineStroke(tickMarkLineStroke);
 		axis2.setAutoRange(false);
 		plot.setRangeAxis(1, axis2);
 		plot.setDataset(1, dataset2);
 		plot.mapDatasetToRangeAxis(1, 1);
+		XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+
+		plot.setRenderer(1, renderer);
+
+		plot.getRendererForDataset(plot.getDataset(1)).setSeriesPaint(0,
+				Color.pink);
+		float dash[] = { 10.0f };
+		plot.getRendererForDataset(plot.getDataset(1)).setBaseOutlineStroke(
+				new BasicStroke(3.0f, BasicStroke.CAP_BUTT,
+						BasicStroke.JOIN_MITER, 10.0f, dash, 0.0f));
+
 		plot.setBackgroundPaint(Color.gray);
 		DateAxis axis = (DateAxis) plot.getDomainAxis();
 		axis.setDateFormatOverride(new SimpleDateFormat("MMM-dd HH:mm"));
