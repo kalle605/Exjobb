a7e6b5151f98aa3a953257f9351b71fd3b6a0f3f
kalle605 <kalle605@live.se>
2015-05-20 13:04:13 +0200

    fsafasfas

--- src/main/filter/F_CO2.java
+++ src/main/filter/F_CO2.java
@@ -33,8 +33,8 @@ public class F_CO2 extends Filter {
 	}
 
 	private void decreasing(double d) {
-		integrator.input(getMax(d, th) == 1 ? -0.5 : -1.5);
-		currentWeight = getMax(d, th) == 1 ? -0.5 : -1.5;
+		integrator.input(getMax(d, th) == 1 ? 0.5 : -1.5);
+		currentWeight = getMax(d, th) == 1 ? 0.5 : -1.5;
 	}
 
 	private void increasing(double d) {
